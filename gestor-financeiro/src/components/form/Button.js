ae7e05eb1dbd0bb3a4b36e199ac24b0552f3bd91
Rafael Menezes <faelgmp@gmail.com>
2023-05-11 13:27:09 -0300

    botão finalizado

---
+++ gestor-financeiro/src/components/form/Button.js
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css"
+
+function Button({handleOnClick, text, styleMode}){
+    return(
+        <button onClick={handleOnClick} className={`${styles.button} ${styles[styleMode]}`} >{text}</button>
+    )
+}
+
+export default Button
--- gestor-financeiro/src/components/pages/Home.js
+++ gestor-financeiro/src/components/pages/Home.js
@@ -1,12 +1,30 @@
+import { useNavigate } from 'react-router-dom'
+
+import logo from "../../assets/logo/logo.svg"
+
+import Button from '../form/Button'
+
 import styles from './Home.module.css'
 
 function Home({showHeader}){
 
     showHeader(false)
-    
+
+    const navigate = useNavigate()
+
+    function toQuestions(){
+        showHeader(true)
+
+        navigate("/questions")
+    }
+
     return(
         <div className={styles.home}>
-            Home
+            <h1>Gestor financeiro</h1>
+            <img src={logo} alt="logo do site" />
+            <h2>Descubra a melhor maneira de gerenciar as suas finanças.</h2>
+
+            <Button handleOnClick={toQuestions} text="Analizar Finanças" styleMode="dark" />
         </div>
     )
 }
